7e0ff9d07a5c47ab3b2a7fcbb90e6fb27ecdfcad
Federico González <federico1818@gmail.com>
2023-01-26 12:09:27 -0300

    add buildStyles funtion in scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
-function defaultTask(cb) {
-    cb()
-}
-  
-exports.default = defaultTask
+'use strict';
+
+const gulp = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+
+function buildStyles() {
+    return gulp
+        .src('./src/assets/scss/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./dist/css'))
+};
+
+exports.default = buildStyles
